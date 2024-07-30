c8642bdd2beb3e63d41a5673f71eda34b7475597
Seungwon_Choi <bestwondev@gmail.com>
2024-07-30 15:20:23 +0900

    Feat: Swagger

---
+++ src/main/java/com/team6/backend/config/SwaggerConfig.java
@@ -0,0 +1,21 @@
+package com.team6.backend.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+
+        Info info = new Info()
+                .version("v1.0.0")
+                .title("Encore Hub API")
+                .description("Encore Hub API 목록입니다.");
+
+        return new OpenAPI()
+                .info(info);
+    }
+}
