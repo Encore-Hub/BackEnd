f5df5072732384487ff6a9f8274b570e40c12f38
kwonkiyong0059 <kkoung425@gmail.com>
2024-08-05 15:43:11 +0900

    공연 관련 좋아요 CRUD 기능

---
+++ src/main/java/com/team6/backend/like/entity/Like.java
@@ -0,0 +1,38 @@
+package com.team6.backend.like.entity;
+
+import com.team6.backend.member.entity.Member;
+import com.team6.backend.pfmc.entity.Pfmc;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@Entity
+@RequiredArgsConstructor
+@Table(name = "likes")
+public class Like {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @ManyToOne
+    @JoinColumn(name = "pfmc_id", nullable = false)
+    private Pfmc pfmc;
+
+    private boolean liked;
+
+    public Like(Member member, Pfmc pfmc, boolean liked) {
+        this.member = member;
+        this.pfmc = pfmc;
+        this.liked = liked;
+    }
+
+    public void toggleLiked() {
+        this.liked = !this.liked;
+    }
+}
