09579e0f4864824adca256266f074db2574fdb9f
USER K <ksar1030k@gmail.com>
2024-08-07 13:58:46 +0900

    디렉토리 이동

--- src/main/java/com/team6/backend/tcommon/config/RestTemplateConfig.java
+++ src/main/java/com/team6/backend/tcommon/config/RestTemplateConfig.java
@@ -1,4 +1,4 @@
-package com.team6.backend.theater.config;
+package com.team6.backend.tcommon.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
