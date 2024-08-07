945d2ad08774814c88e83272edb6a8def5594bb2
USER K <ksar1030k@gmail.com>
2024-08-08 08:59:19 +0900

    |Fix|FavoriteTheaterRequestDto 수정

--- src/main/java/com/team6/backend/favorite/theater/dto/FavoriteTheaterRequestDto.java
+++ src/main/java/com/team6/backend/favorite/theater/dto/FavoriteTheaterRequestDto.java
@@ -6,4 +6,9 @@
     public class FavoriteTheaterRequestDto {
         private Long memberId;
         private String theaterId; // 수정된 부분
+
+        public FavoriteTheaterRequestDto(Long memberId, String theaterId) {
+            this.memberId = memberId;
+            this.theaterId = theaterId;
+        }
     }
