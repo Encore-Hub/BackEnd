da59e037ce5d9bb5cd6ab37b7f67ed2fffe3ce58
kwonkiyong0059 <kkoung425@gmail.com>
2024-08-12 23:16:17 +0900

    Refactor: memberId로 확인 → JWT 토큰에서 추출한 email로 member 확인

--- src/main/java/com/team6/backend/comment/dto/CommentResponseDto.java
+++ src/main/java/com/team6/backend/comment/dto/CommentResponseDto.java
@@ -11,17 +11,17 @@ public class CommentResponseDto {
 
     private Long id;
     private String content;
-    private Long memberId;
+    private String email;
     private String mt20id;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
     private Long parentCommentId; // 부모 댓글 ID
 
-    public CommentResponseDto(Long id, String content, Long memberId, String mt20id,
+    public CommentResponseDto(Long id, String content, String email, String mt20id,
                               LocalDateTime createdAt, LocalDateTime updatedAt, Long parentCommentId) {
         this.id = id;
         this.content = content;
-        this.memberId = memberId;
+        this.email = email; // 이메일 필드로 변경
         this.mt20id = mt20id;
         this.createdAt = createdAt;
         this.updatedAt = updatedAt;
