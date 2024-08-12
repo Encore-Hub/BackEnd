58918362b563c0cd03bbd4675d13a3e649ee2c85
seohee-P <p03061180@gmail.com>
2024-08-12 14:38:21 +0900

    feat: Security 수정 및 UserDetails 구현

--- src/main/java/com/team6/backend/security/config/WebSecurityConfig.java
+++ src/main/java/com/team6/backend/security/config/WebSecurityConfig.java
@@ -1,17 +1,25 @@
 package com.team6.backend.security.config;
 
+import com.team6.backend.refreshToken.repository.RefreshTokenRepository;
+import com.team6.backend.security.UserDetailsServiceImpl;
+import com.team6.backend.security.jwt.JwtAuthenticationFilter;
+import com.team6.backend.security.jwt.JwtAuthorizationFilter;
+import com.team6.backend.security.jwt.JwtUtil;
 import jakarta.annotation.Nullable;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
@@ -23,49 +31,84 @@ import java.util.Collections;
 @RequiredArgsConstructor
 public class WebSecurityConfig {
 
+    private final JwtUtil jwtUtil;
+    private final UserDetailsServiceImpl userDetailsService;
+    private final AuthenticationConfiguration authenticationConfiguration;
+    private final RefreshTokenRepository refreshTokenRepository;
+    private final CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
+    private final CustomAccessDeniedHandler customAccessDeniedHandler;
+
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
     @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter() throws Exception {
+        JwtAuthenticationFilter filter = new JwtAuthenticationFilter(jwtUtil, refreshTokenRepository);
+        filter.setAuthenticationManager(authenticationManager(authenticationConfiguration));
+        return filter;
+    }
+
+    @Bean
+    public JwtAuthorizationFilter jwtAuthorizationFilter() {
+        return new JwtAuthorizationFilter(jwtUtil, userDetailsService, refreshTokenRepository);
+    }
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(csrf -> csrf.disable())
 
-                .cors((corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
+        http.csrf(csrf -> csrf.disable());
 
-                    @Override
-                    public CorsConfiguration getCorsConfiguration(@Nullable HttpServletRequest request) {
-                        CorsConfiguration configuration = new CorsConfiguration();
+        http.cors((corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
 
-                        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:5173"));
-                        configuration.setAllowedMethods(Collections.singletonList("*"));
-                        configuration.setAllowCredentials(true);
-                        configuration.setAllowedHeaders(Collections.singletonList("*"));
-                        configuration.setMaxAge(3600L);
+            @Override
+            public CorsConfiguration getCorsConfiguration(@Nullable HttpServletRequest request) {
+                CorsConfiguration configuration = new CorsConfiguration();
 
-                        configuration.setExposedHeaders(Arrays.asList("AccessToken", "RefreshToken"));
-                        return configuration;
+                configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:5173"));
+                configuration.setAllowedMethods(Collections.singletonList("*"));
+                configuration.setAllowCredentials(true);
+                configuration.setAllowedHeaders(Collections.singletonList("*"));
+                configuration.setMaxAge(3600L);
 
-                    }
-                })))
+                configuration.setExposedHeaders(Arrays.asList("AccessToken", "RefreshToken"));
+                return configuration;
 
-                .sessionManagement(sessionManagement ->
-                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                )
+            }
+        })));
 
-                .authorizeHttpRequests(authorizeHttpRequests ->
-                        authorizeHttpRequests
-                                .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
-                                .requestMatchers("/swagger", "/swagger-ui.html", "/swagger-ui/**", "/api-docs", "/api-docs/**", "/v3/api-docs/**").permitAll()
-                                .requestMatchers("/api/member/**").permitAll()
-                                .requestMatchers("/api/theater/**").permitAll()  // Exclude /api/theater/** from security
-                                .requestMatchers("/api/theaters/**").permitAll()  // Exclude /api/theaters/** from security
-                                .requestMatchers("/api/region/**").permitAll()   // Exclude /api/region/** from security
-                                .anyRequest().authenticated()
-                )
-        ;
+        http.sessionManagement(sessionManagement ->
+                sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        );
+
+        http.authorizeHttpRequests(authorizeHttpRequests ->
+                authorizeHttpRequests
+                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                        .requestMatchers("/swagger", "/swagger-ui.html", "/swagger-ui/**", "/api-docs", "/api-docs/**", "/v3/api-docs/**").permitAll()
+                        .requestMatchers("/api/member/**").permitAll()
+                        //.requestMatchers("/api/theater/**").permitAll()  // Exclude /api/theater/** from security
+                        //.requestMatchers("/api/theaters/**").permitAll()  // Exclude /api/theaters/** from security
+                        .requestMatchers("/api/region/**").permitAll()   // Exclude /api/region/** from security
+                        .anyRequest().authenticated()
+        );
+
+        // 필터 관리
+        http.addFilterBefore(jwtAuthorizationFilter(), JwtAuthenticationFilter.class);
+        http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+        
+        http.exceptionHandling((exceptionConfig) ->
+                exceptionConfig.authenticationEntryPoint(customAuthenticationEntryPoint)
+                        .accessDeniedHandler(customAccessDeniedHandler)
+        );
+
+
 
         return http.build();
     }
