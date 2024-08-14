f89edf77894653564f902cdb5b23ba9c9e51ef47
straycat2 <rlaehgks2@naver.com>
2024-08-14 15:37:55 +0900

    Fix3 : 카카오 회원가입 가능 및 수정

--- src/main/java/com/team6/backend/Oauth2/kakao/controller/KakaoController.java
+++ src/main/java/com/team6/backend/Oauth2/kakao/controller/KakaoController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import jakarta.servlet.http.Cookie;
 
-import java.util.Map;
 
 
 @RestController
--- src/main/java/com/team6/backend/Oauth2/kakao/service/KakaoService.java
+++ src/main/java/com/team6/backend/Oauth2/kakao/service/KakaoService.java
@@ -21,8 +21,7 @@ import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
-import java.util.HashMap;
-import java.util.Map;
+
 
 import java.net.URI;
 
