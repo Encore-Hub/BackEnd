228a52cbd16bbd006f01b20d391de700ea4bd1aa
straycat2 <rlaehgks2@naver.com>
2024-08-16 11:51:24 +0900

    Feat : 스케쥴러 및 공연 데이터 3->9개월 수정

--- src/main/java/com/team6/backend/scheduler/Scheduler.java
+++ src/main/java/com/team6/backend/scheduler/Scheduler.java
@@ -1,6 +1,8 @@
-package com.team6.backend.theater.scheduler;
+package com.team6.backend.scheduler;
 
 
+import com.team6.backend.pfmc.api.PfmcApi;
+import com.team6.backend.pfmc.api.PfmcListApi;
 import com.team6.backend.theater.api.service.TheaterDetailService;
 import com.team6.backend.theater.api.service.TheaterListService;
 import com.team6.backend.theater.api.service.TheaterPfmcDetailService;
@@ -9,7 +11,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
-public class TheaterScheduler {
+public class Scheduler {
 
     @Autowired
     private TheaterListService theaterListService;
@@ -20,10 +22,20 @@ public class TheaterScheduler {
     @Autowired
     private TheaterPfmcDetailService theaterPfmcDetailService;
 
-    @Scheduled(cron = "0 0 0 * * sun") // 매주 월요일 자정에 실행
+    @Autowired
+    private PfmcListApi pfmclistApi;
+
+    @Autowired
+    private PfmcApi pfmcapi ;
+
+    @Scheduled(cron = "0 0 12 * * *") // 매일 낮 12시 에 실행
     public void scheduleTheaterUpdates() throws Exception {
+
+        pfmclistApi.callPfmcListApiJson();
+        pfmcapi.callPfmcApiJson();
         theaterListService.fetchAndSaveTheaterList();
         theaterDetailService.fetchAndSaveTheaterDetails();
         theaterPfmcDetailService.fetchAndSaveTheaterPfmcDetails();
+
     }
 }
